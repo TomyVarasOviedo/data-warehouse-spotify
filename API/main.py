a7ce76d56385b7e7c4165798c174623d5108c201
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2025-10-04 22:08:17 -0300

    add:Gestion del Token completada, faltan endpoints

--- API/main.py
+++ API/main.py
@@ -1,76 +1,88 @@
 import os
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from dotenv import load_dotenv
 import requests
 
 headers = {
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "application/json",
-    "Authorization Bearer": "",
 }
 
 
-# Modelo de petición a la API de Spotify
-class SpotifyAPIRequest(BaseModel):
-    def __init__(self, client_id: str | None, client_secret: str | None):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.token = None
+def get_token(client_id: str, client_secret: str) -> str | None:
+    try:
+        token = requests.post(
+            "https://accounts.spotify.com/api/token",
+            headers=headers,
+            data={"grant_type": "client_credentials"},
+            auth=(client_id, client_secret),
+        )
+        token.raise_for_status()
+        token = token.json()
+        token = token["access_token"]
+        headers["Authorization"] = f"Bearer {token}"
+        return token
 
-    def set_token(self, token: str):
-        self.token = token
+    except HTTPException as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        return None
 
-    def get_token(self) -> str | None:
-        crendentials = {
-            "grant_type": "client_credentials",
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-        }
-        try:
-            token = requests.post(
-                "https://accounts.spotify.com/api/token",
-                headers=headers,
-                json=crendentials,
-            )
-            if token.status_code == 200:
-                token_json = token.json()
-                self.set_token(token_json["access_token"])
-                headers["Authorization Bearer"] = f"{self.token}"
-                return self.token
-            else:
-                raise HTTPException(token.status_code)
+    except requests.RequestException as req_err:
+        print(f"Request error occurred: {req_err}")
+        return None
 
-        except HTTPException as http_err:
-            print(f"HTTP error occurred: {http_err}")
-            return None
 
-        except Exception as e:
-            print(f"Error al obtener token: {e}")
-            return None
-
+# Cargar variables de entorno
+load_dotenv()
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+TOKEN = get_token(str(client_id), str(client_secret))
 
 app = FastAPI()
+# ------------------------#
+# --- Rutas de la API --- #
+# ------------------------#
 
 
 @app.get("/track-id/{track_id}")
 async def get_track_info(track_id: str):
-    pass
+    try:
+        peticion = requests.get(
+            f"https://api.spotify.com/v1/tracks/{track_id}", headers=headers
+        )
+        if peticion.status_code == 200:
+            return peticion.json()
+    except HTTPException as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+    except Exception as e:
+        print(f"Error al realizar la petición: {e}")
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
 
 
 @app.get("/artist-id/{artist_id}")
 async def get_artist_info(artist_id: str):
     # Implementar API de Spotify para buscar id de artista
-    pass
+    try:
+        peticion = requests.get(
+            f"https://api.spotify.com/v1/artists/{artist_id}", headers=headers
+        )
+        if peticion.status_code == 200:
+            return peticion.json()
 
+    except HTTPException as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+    except Exception as e:
+        print(f"Error al realizar la petición: {e}")
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+
+
+# ------------------------#
+# --- Rutas de la API --- #
+# ------------------------#
 
 if __name__ == "__main__":
     import uvicorn
 
-    load_dotenv()
-    client_id = os.getenv("client_id")
-    client_secret = os.getenv("client_secret")
-    print(client_id, client_secret)
-    TOKEN = SpotifyAPIRequest(str(client_id), str(client_secret))
-    TOKEN.get_token()
     uvicorn.run(app, host="0.0.0.0", port=8000)
