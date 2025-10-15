db5307682a88834242cbf222328e69dbb42e6104
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2025-10-15 10:28:44 -0300

    add: API terminada y devuelve lo acorde a las entidades

--- API/main.py
+++ API/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
@@ -51,7 +52,16 @@ async def get_track_info(track_id: str):
             f"https://api.spotify.com/v1/tracks/{track_id}", headers=headers
         )
         if peticion.status_code == 200:
-            return peticion.json()
+            response = json.loads(peticion.text)
+
+            return {
+                "track_id": response["id"],
+                "track_name": response["name"],
+                "artists": [artist["id"] for artist in response["artists"]],
+                "album_id": response["album"]["id"],
+                "duration_ms": response["duration_ms"],
+            }
+
     except HTTPException as http_err:
         print(f"HTTP error occurred: {http_err}")
         raise HTTPException(status_code=500, detail="Error interno del servidor")
@@ -68,7 +78,12 @@ async def get_artist_info(artist_id: str):
             f"https://api.spotify.com/v1/artists/{artist_id}", headers=headers
         )
         if peticion.status_code == 200:
-            return peticion.json()
+            response = json.loads(peticion.text)
+            return {
+                "artist_id": response["id"],
+                "artist_name": response["name"],
+                "followers": response["followers"]["total"],
+            }
 
     except HTTPException as http_err:
         print(f"HTTP error occurred: {http_err}")
@@ -86,7 +101,12 @@ async def get_album_info(album_id: str):
             f"https://api.spotify.com/v1/albums/{album_id}", headers=headers
         )
         if peticion.status_code == 200:
-            return peticion.json()
+            response = json.loads(peticion.text)
+            return {
+                "album_id": response["id"],
+                "album_name": response["name"],
+                "release_date": response["release_date"],
+            }
     except HTTPException as http_err:
         print(f"HTTP error occurred: {http_err}")
         raise HTTPException(status_code=500, detail="Error interno del servidor")
